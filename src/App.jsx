1cb8952dab4b76271eacd803c357dd74e400cfab
MMuzzamil1 <124052423+MMuzzamil1@users.noreply.github.com>
2024-03-07 15:54:28 +0500

    Written jsx for navbar and imported fonts

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,22 @@ function App() {
 
   return (
     <>
-     Hello World
+     <nav>
+      <div className='logo'>
+        <img src="/images/brand_logo.png" alt="logo" />
+      </div>
+
+      <ul>
+        <li href= "#">Menu</li>
+        <li href= "#">Location</li>
+        <li href= "#">About</li>
+        <li href= "#">Contact</li>
+      </ul>
+
+      <button>
+        login
+      </button>
+     </nav>
     </>
   )
 }
