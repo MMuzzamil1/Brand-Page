9f1c1a8de26ad92ddf6d5995b17d1917f6e7c48e
MMuzzamil1 <124052423+MMuzzamil1@users.noreply.github.com>
2024-03-08 14:37:46 +0500

    Wrote css of nav bar and created 'Navigation' component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <nav>
-      <div className='logo'>
-        <img src="/images/brand_logo.png" alt="logo" />
-      </div>
-
-      <ul>
-        <li href= "#">Menu</li>
-        <li href= "#">Location</li>
-        <li href= "#">About</li>
-        <li href= "#">Contact</li>
-      </ul>
-
-      <button>
-        login
-      </button>
-     </nav>
+    <Navigation/>
     </>
   )
 }
